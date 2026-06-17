16d6bb933bc19f1208875a0730aeb4e52080e6c9
Udo <udo@local>
2026-06-17 05:42:03 +0200

    chore: add app healthchecks

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './auth/public.decorator';
 import { AppService } from './app.service';
 
 @Controller('health')
+@Public()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
